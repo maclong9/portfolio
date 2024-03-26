af7bf35ebf50b68006c539c93f1dd2748792abc0
Mac <maclong9@icloud.com>
2024-03-26 16:33:15 +0000

    Update scroll.js

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -3,20 +3,11 @@ let lastScrollTop = 0;
 window.addEventListener("scroll", function () {
   let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
   let header = document.querySelector("header");
-  let span = document.querySelector("#tagline");
 
   if (currentScroll > lastScrollTop && currentScroll > 500) {
     header.classList.add("hidden");
   } else {
     header.classList.remove("hidden");
-  }
-
-  if (currentScroll > 470) {
-    header.classList.add("scrolled");
-    span.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-    span.classList.remove("scrolled");
   }
 
   lastScrollTop = currentScroll;
