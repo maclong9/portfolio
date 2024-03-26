75cf4caed50bc83f79ecb8aa31a09040330a34cc
Mac <maclong9@icloud.com>
2024-03-26 17:21:27 +0000

    Update meta.js

--- scripts/components/meta.js
+++ scripts/components/meta.js
@@ -1,10 +1,14 @@
-export class MetaData extends HTMLElement {
+export class CustomMeta extends HTMLElement {
   connectedCallback() {
     const title = this.getAttribute('title');
     const description = this.getAttribute('description');
     const image = this.getAttribute('image');
     const type = this.getAttribute('type');
     const url = this.getAttribute('url');
+
+    this.createMetaTag('charset', 'UTF-8');
+    this.createMetaTag('name', 'viewport', 'content', 'width=device-width, initial-scale=1.0');
+    this.createMetaTag('title', title);
 
     this.createMetaTag('og:title', title);
     this.createMetaTag('og:description', description);
@@ -19,10 +23,14 @@ export class MetaData extends HTMLElement {
     this.createMetaTag('twitter:url', url);
   }
 
-  createMetaTag(name, content) {
+  createMetaTag(tagName, attribute, content) {
     const metaTag = document.createElement('meta');
-    metaTag.setAttribute('property', name);
-    metaTag.setAttribute('content', content);
+    if (content) {
+      metaTag.setAttribute(attribute, content);
+    } else {
+      metaTag.setAttribute('charset', 'UTF-8');
+      metaTag.setAttribute('name', tagName);
+    }
     document.head.appendChild(metaTag);
   }
 }
