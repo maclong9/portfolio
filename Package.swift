89638a72b4cc0f6fbc81597bbeb11d6b3d249b20
Mac <maclong9@icloud.com>
2025-04-02 15:16:53 +0100

    chore: update web-ui package version

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ let package = Package(
   name: "Portfolio",
   platforms: [.macOS(.v13)],
   dependencies: [
-    //    .package(url: "https://github.com/maclong9/web-ui", from: "0.0.4")
-    .package(path: "../web-ui")
+        .package(url: "https://github.com/maclong9/web-ui", from: "0.0.6")
+//    .package(path: "../web-ui")
   ],
   targets: [
     .executableTarget(
