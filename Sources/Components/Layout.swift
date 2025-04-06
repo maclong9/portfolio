288e8eefc909d73e01b79aa54795082b9b9a935b
Mac <maclong9@icloud.com>
2025-04-06 14:12:25 +0100

    feat: improve image styles

--- Sources/Components/Layout.swift
+++ Sources/Components/Layout.swift
@@ -43,7 +43,7 @@ struct Layout: HTML {
           Heading(level: .one) { heading }.styled(size: .xl4)
             .margins(.bottom)
           if let image = image {
-            Image(source: image, description: "\(heading) cover", classes: ["w-full h-52 sm:h-80"])
+            Image(source: "\(image)", description: "\(heading) cover", classes: ["w-full h-52 sm:h-80 rounded-lg shadow-lg"])
           }
           Text { description }.margins(.top)
         }
