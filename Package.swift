c0cb94e787a1ee34e3af3bedec18ebc84cf12d03
Mac <maclong9@icloud.com>
2025-04-15 00:33:56 +0100

    fix: remove product from manifest

--- Package.swift
+++ Package.swift
@@ -6,9 +6,6 @@ import PackageDescription
 let package = Package(
   name: "Portfolio",
   platforms: [.macOS(.v13)],
-  products: [
-    .executable(name: "Portfolio", targets: ["Portfolio"])
-  ],
   dependencies: [
     .package(url: "https://github.com/maclong9/web-ui", branch: "main"),
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
