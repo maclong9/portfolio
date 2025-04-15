e174c7d1d831535b150bdee8827d8ab555d4a3df
Mac <maclong9@icloud.com>
2025-04-16 00:44:40 +0100

    fix: trailing commas

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/maclong9/web-ui", from: "1.0.1"),
-    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
+    .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0")
   ],
   targets: [
     .executableTarget(
@@ -27,7 +27,7 @@ let package = Package(
       name: "PortfolioSnippets",
       dependencies: [
         .product(name: "WebUI", package: "web-ui")
-      ],
+      ]
     )
   ]
 )
