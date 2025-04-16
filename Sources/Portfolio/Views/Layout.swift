0df339c389ae2c8e9f89119a373bfe57c1327f7c
Mac <maclong9@icloud.com>
2025-04-16 16:47:32 +0100

    feat: increase max-w slightly

--- Sources/Portfolio/Views/Layout.swift
+++ Sources/Portfolio/Views/Layout.swift
@@ -38,7 +38,7 @@ struct Layout: HTML {
       .flex(grow: .one)
       .margins(.horizontal, auto: true)
       .frame(maxWidth: .custom("95vw"))
-      .frame(maxWidth: .character(64), on: .sm)
+      .frame(maxWidth: .fixed(160), on: .sm)
       .font(wrapping: .pretty)
       .padding()
 
