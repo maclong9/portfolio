db9701f7a1ccb39f11fa26fdaf8f75069a56a919
Mac Long <maclong9@icloud.com>
2025-04-23 00:58:03 +0100

    fix: update to swift version 6.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,16 +7,16 @@ let package = Package(
   name: "Portfolio",
   platforms: [.macOS(.v13)],
   products: [
-    .executable(name: "Portfolio", targets: ["Portfolio"])
+    .executable(name: "Portfolio", targets: ["Portfolio"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/maclong9/web-ui.git", from: "1.0.0")
+    .package(url: "https://github.com/maclong9/web-ui.git", from: "1.0.0"),
   ],
   targets: [
     .executableTarget(
       name: "Portfolio",
       dependencies: [
-        .product(name: "WebUI", package: "web-ui")
+        .product(name: "WebUI", package: "web-ui"),
       ],
       resources: [
         .copy("Public"),
