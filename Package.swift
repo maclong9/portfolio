3f864ee4d3a7cb5c627c289b1ac9b6fced9364f3
Mac <maclong9@icloud.com>
2025-04-22 19:58:26 +0100

    fix: package resolved to correct web-ui version

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     .executable(name: "Portfolio", targets: ["Portfolio"])
   ],
   dependencies: [
-    .package(url: "https://github.com/maclong9/web-ui", from: "1.0.0")
+    .package(url: "https://github.com/maclong9/web-ui.git", from: "1.0.0")
   ],
   targets: [
     .executableTarget(
